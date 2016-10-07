ec890550725f1ee35e75c973ca4864a0688db863
Arne Riekert <arneparne@Arne-Riekerts-MacBook-Pro.local>
2016-10-07 16:14:45 +0200

    Laat de huidige datum en tijd zien

---
+++ CurrentDate.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+class GetDate {
+	public static void main(String[] args) {
+		LocalDateTime dateTime = LocalDateTime.now();
+		System.out.println("\n" + dateTime + "\n");
+	}
+}
