f39a4a7e417045c415f1d105a1091f96d0f0a72a
Arne Riekert <arneparne@gmail.com>
2016-11-06 18:15:30 +0100

    Endless pressing of button jeej

--- Smile/Smile.java
+++ Smile/Smile.java
@@ -6,7 +6,6 @@ public class Smile {
 	static JFrame frame = new JFrame("Smile");
 	static DrawPanel dP;
 	static JButton smileButton;
-	static boolean msg = false;
 
 	public static void main(String[] args) {
 		Smile smile = new Smile();
@@ -17,7 +16,7 @@ public class Smile {
 		dP = new DrawPanel();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		smileButton = new JButton("Smile!");
-		smileButton.addActionListener(new DrawPanel());
+		smileButton.addActionListener(dP);
 		frame.getContentPane().add(BorderLayout.SOUTH, smileButton);
 		frame.getContentPane().add(BorderLayout.CENTER, dP);
 		frame.setSize(800, 600);
@@ -29,24 +28,31 @@ public class Smile {
 	}
 
 	class DrawPanel extends JPanel implements ActionListener {
+		boolean msg = false;
 
 		public void actionPerformed (ActionEvent event) {
-			msg = true;
 			frame.repaint();
 			System.out.println("button pressed");
+			if (msg) {
+				msg = false;
+			} else if (!msg) {
+				msg = true;
+			}
 		}
 
 		public void paintComponent (Graphics g) {
-			g.setColor(new Color(100,0,0));
-			g.fillRect(0,0,this.getWidth(), this.getHeight());
 			System.out.println("Loading drawpanel");
 			System.out.println(msg);
+
+			if (!msg) {
+				g.setColor(new Color(100,0,0));
+				g.fillRect(0,0,this.getWidth(), this.getHeight());
+			}
 
 			if (msg) {
 				System.out.println("image");
 				Image image = new ImageIcon("smile.jpg").getImage();
 				g.drawImage(image, 0, 0, this);
-				msg = false;
 			}
 		}
 	}
